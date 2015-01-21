85632eab7295bb2e75e0d0112e6a5714d3f8f687
Marcin Kuptel <marcinkuptel@gmail.com>
2015-01-21 17:09:50 +0100

    Initial commit

--- Realm/ViewController.swift
+++ Realm/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Realm
+
+class DemoObject: RLMObject {
+    dynamic var myProp = ""
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        RLMRealm.defaultRealm().transactionWithBlock { () -> Void in
+            println("Creating demo object")
+            DemoObject.createInDefaultRealmWithObject(["hello!"])
+        }
+        
+        for object in DemoObject.allObjects() {
+            println(object.description)
+        }
     }
 
     override func didReceiveMemoryWarning() {
